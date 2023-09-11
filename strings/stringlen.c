03ce4f439cd5cd210af1b899ed97d62bf94d74c5
Tomilade18 <jboyyumzey@gmail.com>
2023-09-11 15:14:14 +0100

    strlen

---
+++ strings/stringlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char name[30];
+    int count = 0, i = 0;
+    printf("enter name:");
+    gets(name);
+    while(name[i] != '\0')
+    {
+        count++;
+        i++;
+    }
+    printf("%d", count);
+}
