f82a980715814c8fbe3d4e2bdc8944f62ceb45f6
Tomilade18 <jboyyumzey@gmail.com>
2023-09-12 00:29:52 +0100

    new without strcat

--- strings/stringcat.c
+++ strings/stringcat.c
@@ -3,9 +3,14 @@
 
 int main()
 {
+    int l1,l2,i;
     char s1[30]="jenny";
     char s2[]="khatri";
-    strcat(s1,s2);
+    // strcat(s1,s2);
+
+    l1 = strlen(s1);
+    l2 = strlen(s2);
+    
     printf("%s\n", s1);
     puts(s2);
 }
